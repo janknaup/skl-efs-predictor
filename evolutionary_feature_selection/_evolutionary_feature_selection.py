9fb84bb4f0eb3a1705c8e72581ed6ba40a2be860
Jan M. Knaup <jan.knaup@gmail.com>
2022-11-23 00:04:30 +0100

    Updated parameter and attribute names to conform to scikit learn conventions
    fixed typo in EvolutionaryFeatureSelection constructor name
    added defaults to all numerical parameters and check_scalar upon storing as parameter members in EvolutionaryFeatureSelection to conform to requirement of parameter not being changes upon initialization
    fixed _mutate failing because of wrong shape of array passed to np.random_state_choice
    fixed _procreate failing because of wrong shape of array passed to np.random_state_choice
    modified _calculate_fitness_specimen to directly index X instead of calling self.transform to avoid temporarily setting current_population_ to an invalid value
    fixed check_X_y call in fit having extraneous ensure_min_features
    moved default initialization of and predictor_, scoring_ attributes to fit to have clean constructor

--- evolutionary_feature_selection/_evolutionary_feature_selection.py
+++ evolutionary_feature_selection/_evolutionary_feature_selection.py
@@ -6,19 +6,20 @@ from sklearn.base import BaseEstimator
 from sklearn.feature_selection import SelectorMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted, check_random_state, check_scalar
 from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
 
 
 class EvolutionaryFeatureSelection(BaseEstimator, SelectorMixin):
     """ A feature selection transformer that selects a set of features of given size by evolutionary optimization
 
     The optimization is performed in a procreation-mutation-selection cycle with the fitness calculated as the
-    validation score of the predictor fitted using a particular selection of features.
+        validation score of the predictor fitted using a particular selection of features.
 
     Parameters
     ----------
     predictor : predictor
         predictor to train on the reduced feature set
-    score : callable
+    scoring : callable
         scoring function to use as fitness function for specimen selection
     n_features : int
         number of features to select per specimen
@@ -39,34 +40,34 @@ class EvolutionaryFeatureSelection(BaseEstimator, SelectorMixin):
     random_state : numpy.random.random_state
         random state for repeatability in testing
 
+    Attributes
+    ----------
+    random_state_ : Numpy random state
+        For testing and consistent parallel processing
+    population_ : Ndarray shape(population_size, n_features_in_)
+        The full population of feature masks in the current iteration/generation
+    fitness_values_ : Ndarray, shape(population_size)
+        The fitness/score values of the population
+    current_specimen_ : Ndarray, shape(n_features_in_)
+        The specimen with the highest fitness value, same as population_[0]
+    fitness_history_ : Ndarray, shape(generations, population_size)
+        Trace of the population fitness values over along all generations for fit debugging and quality assessment
     """
 
-    def __int__(self, predictor, scoring=None, n_features=None,
-                population_size=10, n_breeders=None, mutation_rate=0.5, n_mutation_features=None,
-                generations=10, initial_population=None, random_state=None):
+    def __init__(self, predictor=None, scoring=None, n_features=1,
+                 population_size=10, n_breeders=5, mutation_rate=0.5, n_mutation_features=1,
+                 generations=10, initial_population=None, random_state=None):
         self.predictor = predictor
-        if scoring is not None:
-            self.scoring = scoring
-        else:
-            self.scoring = r2_score
-        self.n_features = n_features
-        self.population_size = population_size
-        self.breeder_fraction = n_breeders
-        if n_breeders is None:
-            self.n_breeders = round(self.n_features * 0.5)
-        self.mutation_rate = mutation_rate
-        if n_mutation_features is None:
-            self.n_mutation_features = round(self.n_features * 0.1)
-        else:
-            self.n_mutation_features = n_mutation_features
-        self.generations = generations
+        self.scoring = scoring
+        self.n_features = check_scalar(n_features, "n_features", int, min_val=1)
+        self.population_size = check_scalar(population_size, "population_size", int, min_val=1)
+        self.n_breeders = check_scalar(n_breeders, "n_breeders", int, min_val=1)
+        self.mutation_rate = check_scalar(mutation_rate, "mutation_rate", float, min_val=0, max_val=1)
+        self.n_mutation_features = check_scalar(n_mutation_features, "n_mutation_features", int, min_val=0,
+                                                max_val=self.n_features)
+        self.generations = check_scalar(generations, "generations", int, min_val=1)
         self.initial_population = initial_population
         self.random_state = random_state
-        # ---------
-        self._population = None
-        self._fitness_values = None
-        self._current_specimen = None
-        self._fitness_history = None
 
     def _mutate(self, specimen):
         """
@@ -85,8 +86,8 @@ class EvolutionaryFeatureSelection(BaseEstimator, SelectorMixin):
         self.random_state_ = check_random_state(self.random_state)
         new_specimen = np.array(specimen)
         for i in range(self.n_mutation_features):
-            new_specimen[self.random_state_.choice(np.argwhere(specimen))] = False
-            new_specimen[self.random_state_.choice(np.argwhere(np.bitwise_not(specimen)))] = True
+            new_specimen[self.random_state_.choice(np.argwhere(specimen).ravel())] = False
+            new_specimen[self.random_state_.choice(np.argwhere(np.bitwise_not(specimen)).ravel())] = True
         return new_specimen
 
     def _procreate(self, father, mother):
@@ -105,7 +106,7 @@ class EvolutionaryFeatureSelection(BaseEstimator, SelectorMixin):
         self.random_state_ = check_random_state(self.random_state)
         offspring = father + mother
         while np.count_nonzero(offspring) > self.n_features:
-            offspring[self.random_state_.choice(np.argwhere(offspring))] = False
+            offspring[self.random_state_.choice(np.argwhere(offspring).ravel())] = False
         return offspring
 
     def _populate(self, n_features_in):
@@ -117,14 +118,14 @@ class EvolutionaryFeatureSelection(BaseEstimator, SelectorMixin):
         n_features_in : int number of features in the input data set
         """
         self.random_state_ = check_random_state(self.random_state)
-        self._population = np.zeros(self.population_size, n_features_in, dtype=bool)
+        self.population_ = np.zeros((self.population_size, n_features_in), dtype=bool)
         if self.initial_population is not None:
             row = self.initial_population.shape[1]
-            self._population[0:min(self.initial_population.shape[1], self.population_size)] = self.initial_population
+            self.population_[0:min(self.initial_population.shape[1], self.population_size)] = self.initial_population
         else:
             row = 0
         while row < self.population_size:
-            self._population[row, self.random_state_.randint(0, n_features_in, self.n_features)] = True
+            self.population_[row, self.random_state_.randint(0, n_features_in, self.n_features)] = True
             row += 1
 
     def _calculate_fitness_values(self, X, y):
@@ -139,56 +140,62 @@ class EvolutionaryFeatureSelection(BaseEstimator, SelectorMixin):
         -------
         array of float fitness values
         """
-        for specimen_index in range(self._population.shape[0]):
-            self._fitness_values[specimen_index] = self._calculate_fitness_specimen(X, y,
-                                                                                    self._population[specimen_index])
-        return self._fitness_values
+        for specimen_index in range(self.population_.shape[0]):
+            self.fitness_values_[specimen_index] = self._calculate_fitness_specimen(X, y,
+                                                                                    self.population_[specimen_index])
+        return self.fitness_values_
 
     def _calculate_fitness_specimen(self, X, y, specimen):
-        self._current_specimen = specimen
-        y_pred = self.predictor.fit(self.transform(X))
-        fitness = self.scoring(y, y_pred)
-        self._current_specimen = self._population[0]
+        local_X = X[:, specimen]
+        y_pred = self.predictor_.fit(local_X, y).predict(local_X)
+        fitness = self.scoring_(y, y_pred)
         return fitness
 
     def _get_support_mask(self):
-        return self._current_specimen
+        return self.current_specimen_
 
     def fit(self, X, y):
-        X, y = check_X_y(X, y, ensure_min_features=self.n_features)
+        X, y = check_X_y(X, y)
+        self.n_features_in_ = X.shape[1]
+        if self.predictor is None:
+            self.predictor_ = LinearRegression()
+        else:
+            self.predictor_ = self.predictor()
+        if self.scoring is None:
+            self.scoring_ = r2_score
+        else:
+            self.scoring_ = self.scoring
         self.random_state_ = check_random_state(self.random_state)
-        if self._population is None:
-            self._populate(X)
-        # set initial fitness values and sort initial speciments by fitness
-        print("Initiating evolutionary feature selection")
-        self._fitness_values = np.zeros(self.population_size, dtype=float)
+        self._populate(X.shape[1])
+        # set initial fitness values and sort initial specimens by fitness
+        self.fitness_values_ = np.zeros(self.population_size, dtype=float)
         self._calculate_fitness_values(X, y)
-        fitness_order = np.argsort(self._fitness_values)
-        self._population = self._population[fitness_order]
-        self._fitness_values = self._fitness_values[fitness_order]
-        print("Initial population fitness scores ({0})".format(self.scoring.__name__))
-        print(self._fitness_values)
+        fitness_order = np.argsort(self.fitness_values_)
+        self.population_ = self.population_[fitness_order]
+        self.fitness_values_ = self.fitness_values_[fitness_order]
+        self.current_specimen_ = self.population_[0]
         # iterate generations
-        self._fitness_history = np.zeros((self.generations + 1, self.population_size))
-        self._fitness_history[0] = self._fitness_values
+        self.fitness_history_ = np.zeros((self.generations + 1, self.population_size))
+        self.fitness_history_[0] = self.fitness_values_
         temp_pop_size = (2 * self.population_size) - self.n_breeders
         converged = False
         generation = 0
         while not converged:
-            temp_pop = np.zeros((temp_pop_size, self._population[0].shape[1]))
-            temp_pop[0:self.population_size] = self._population
+            temp_pop = np.zeros((temp_pop_size, self.n_features_in_), dtype=bool)
+            temp_pop[0:self.population_size] = self.population_
             temp_fitness_values = np.zeros(temp_pop_size)
-            temp_fitness_values[0:self.population_size] = self._fitness_values
-            for offspring_idx in range(self.population_size, temp_pop_size + 1):
+            temp_fitness_values[0:self.population_size] = self.fitness_values_
+            for offspring_idx in range(self.population_size, temp_pop_size):
                 dad, mom = self.random_state_.randint(self.n_breeders, size=2)
-                temp_pop[offspring_idx] = self._procreate(self._population[dad], self._population[mom])
+                temp_pop[offspring_idx] = self._procreate(self.population_[dad], self.population_[mom])
                 if self.random_state_.rand() <= self.mutation_rate:
                     temp_pop[offspring_idx] = self._mutate(temp_pop[offspring_idx])
-                temp_fitness_values[offspring_idx] = self._calculate_fitness_specimen(temp_pop[offspring_idx])
+                temp_fitness_values[offspring_idx] = self._calculate_fitness_specimen(X, y, temp_pop[offspring_idx])
             fitness_order = np.argsort(temp_fitness_values)[0:self.population_size]
-            self._population = temp_pop[fitness_order]
-            self._fitness_values = temp_fitness_values[fitness_order]
-            self._fitness_history[generation + 1] = self._fitness_values
+            self.population_ = temp_pop[fitness_order]
+            self.fitness_values_ = temp_fitness_values[fitness_order]
+            self.current_specimen_ = self.population_[0]
+            self.fitness_history_[generation + 1] = self.fitness_values_
             generation += 1
             if generation >= self.generations:
                 converged = True
