cdadb3f7c9580830f05e47d4921f5bccb014d934
Jan M. Knaup <jan.knaup@gmail.com>
2022-12-19 02:33:19 +0100

    added trace utilities module and 2 trace plotting functions: fitness_trace to plot the development of population fitness during evolution feature_trace to plot the composition of the population during evolution

--- evolutionary_feature_selection/__init__.py
+++ evolutionary_feature_selection/__init__.py
@@ -1,5 +1,6 @@
 from ._evolutionary_feature_selection import EvolutionaryFeatureSelection
+from ._trace_utilities import fitness_trace, feature_trace
 
 from ._version import __version__
 
-__all__ = ['EvolutionaryFeatureSelection', '__version__']
+__all__ = ['EvolutionaryFeatureSelection', 'fitness_trace', 'feature_trace', '__version__']
