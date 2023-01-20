bb98203b6e6d9cf83739a72602fcea4732044c10
Jan M. Knaup <jan.knaup@gmail.com>
2023-01-20 01:04:52 +0100

    added testing of plots generated by trace utils this adds a test requirement for pytest-mpl and changes the pytest command line

--- evolutionary_feature_selection/tests/test_trace_utilities.py
+++ evolutionary_feature_selection/tests/test_trace_utilities.py
@@ -17,19 +17,25 @@ def efs():
     return EvolutionaryFeatureSelection(generations=50, n_features=5, population_size=20, n_breeders=8,
                                         mutation_rate=0.1, random_state=1337)
 
+@pytest.mark.mpl_image_compare
 def test_fitness_trace(data, efs):
     efs.fitness_trace = True
     efs.fit(*data)
     trace_fig = fitness_trace(efs.fitness_history_, top_specimen=True, all_specimens=True, mean=True, range_area=True)
+    return trace_fig
 
+@pytest.mark.mpl_image_compare
 def test_feature_trace(data, efs):
     efs.population_trace = True
     efs.fit(*data)
     top_specimen_trace = feature_trace(efs.population_history_, specimen='top')
     all_specimen_trace = feature_trace(efs.population_history_, specimen='all',
                                        feature_names=['X{0:02d}'.format(i) for i in range(len(data[1]))])
+    return all_specimen_trace
 
+@pytest.mark.mpl_image_compare
 def test_fitness_diversity_trace(data, efs):
     efs.fitness_trace = True
     efs.fit(*data)
     trace_fig = fitness_diversity_trace(efs.fitness_history_, std=True, range=True)
+    return trace_fig
